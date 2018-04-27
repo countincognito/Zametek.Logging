91a4a29a86e037d12871339fac70ad2d292613aa
Riccardo M Bennett-Lovsey <countincognito@outlook.com>
2018-04-27 22:34:38 +0100

    Add LogProxy create method to return objects.

--- Zametek.Utility.Logging/LogProxy.cs
+++ Zametek.Utility.Logging/LogProxy.cs
@@ -14,7 +14,8 @@ namespace Zametek.Utility.Logging
         public static I Create<I>(
             I instance,
             ILogger logger,
-            LogType logType = LogType.All) where I : class
+            LogType logType = LogType.All,
+            params IInterceptor[] extraInterceptors) where I : class
         {
             if (instance == null)
             {
@@ -26,26 +27,72 @@ namespace Zametek.Utility.Logging
             }
 
             Debug.Assert(typeof(I).IsInterface);
+            List<IInterceptor> interceptors = BuildStandardInterceptors(instance, logger, logType);
 
-            bool useAll = logType == LogType.All;
+            if (extraInterceptors != null && extraInterceptors.Any())
+            {
+                interceptors.AddRange(extraInterceptors);
+            }
+
+            return s_ProxyGenerator.CreateInterfaceProxyWithTargetInterface(instance, interceptors.ToArray());
+        }
+
+        public static object Create(
+            Type instanceType,
+            object instance,
+            ILogger logger,
+            LogType logType = LogType.All,
+            params IInterceptor[] extraInterceptors)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException(nameof(instance));
+            }
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
+            Debug.Assert(instanceType.IsInterface);
+            List<IInterceptor> interceptors = BuildStandardInterceptors(instance, logger, logType);
+
+            if (extraInterceptors != null && extraInterceptors.Any())
+            {
+                interceptors.AddRange(extraInterceptors);
+            }
+
+            return s_ProxyGenerator.CreateInterfaceProxyWithTargetInterface(instanceType, instance, interceptors.ToArray());
+        }
+
+        private static List<IInterceptor> BuildStandardInterceptors(object instance, ILogger logger, LogType logType)
+        {
+            if (instance == null)
+            {
+                throw new ArgumentNullException(nameof(instance));
+            }
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
             var interceptors = new List<IInterceptor>();
 
-            if (useAll || logType.HasFlag(LogType.Tracking))
+            if (logType.HasFlag(LogType.Tracking))
             {
                 interceptors.Add(new AsyncTrackingInterceptor().ToInterceptor());
             }
 
-            if (useAll || logType.HasFlag(LogType.Error))
+            if (logType.HasFlag(LogType.Error))
             {
                 interceptors.Add(new AsyncErrorLoggingInterceptor(logger).ToInterceptor());
             }
 
-            if (useAll || logType.HasFlag(LogType.Performance))
+            if (logType.HasFlag(LogType.Performance))
             {
                 interceptors.Add(new AsyncPerformanceLoggingInterceptor(logger).ToInterceptor());
             }
 
-            if (useAll || logType.HasFlag(LogType.Diagnostic))
+            if (logType.HasFlag(LogType.Diagnostic))
             {
                 // Check for NoDiagnosticLogging Class scope.
                 bool classHasNoDiagnosticAttribute = instance.GetType().GetCustomAttributes(typeof(NoDiagnosticLoggingAttribute), false).Any();
@@ -56,7 +103,7 @@ namespace Zametek.Utility.Logging
                 }
             }
 
-            return s_ProxyGenerator.CreateInterfaceProxyWithTargetInterface(instance, interceptors.ToArray());
+            return interceptors;
         }
     }
 }
--- Zametek.Utility.Logging/LogType.cs
+++ Zametek.Utility.Logging/LogType.cs
@@ -5,10 +5,11 @@ namespace Zametek.Utility.Logging
     [Flags]
     public enum LogType
     {
-        All = 0,
+        None = 0,
         Tracking = 1 << 0,
         Error = 1 << 1,
         Performance = 1 << 2,
         Diagnostic = 1 << 3,
+        All = Tracking | Error | Performance | Diagnostic,
     }
 }
